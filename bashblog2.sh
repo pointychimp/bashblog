92f7b48e143acb72666a56fa4a1e73726d6fbf4f
Matt Traudt <matt@server.(none)>
2014-02-18 16:55:37 -0600

    Halfway done with function for creating an html file
    Fixed tabbing
    
    Adding funcs for creating css and header/footer from scratch if necessary
    
    Added initialize func and wrapped some startup stuff in it

--- bashblog2.sh
+++ bashblog2.sh
@@ -53,16 +53,18 @@ initializeGlobalVariables() {
     global_email="johndoe@example.com" # your email
     global_license="CC by-nc-nd" # "&copy;" for copyright, for example
    
-    global_indexFile="index.html" # index page, best to leave alone
-    global_archiveFile="archive.html" # page to list all posts
-    global_headerFile="header.html" # header file  # change to use something
-    global_footerFile="footer.html" # footer file  # other than default
-    
     global_sourceDir="source" # dir for easy-to-edit source             # best  
     global_draftsDir="drafts" # dir for drafts                          # to
     global_htmlDir="html" # dir for final html files                    # leave
     global_previewDir="$global_htmlDir/preview" # dir for previews      # these
     global_tempDir="/tmp/$global_softwareName" # dir for pending files  # alone
+   
+    global_indexFile="index.html" # index page, best to leave alone
+    global_archiveFile="archive.html" # page to list all posts, best to leave alone
+    global_headerFile=".header.html" # header file 
+    global_footerFile=".footer.html" # footer file 
+    global_blogcssFile="blog.css" # blog's styling
+   
     
     global_feed="feed.rss" # rss feed file
     global_feedLength="10" # num of articles to include in feed
@@ -103,14 +105,14 @@ testMarkdown() {
 #
 # takes no args
 detectDateVersion() {
-	date --version >/dev/null 2>&1
-	if [[ $? -ne 0 ]];  then
-		# date utility is BSD. Test if gdate is installed
-		if gdate --version >/dev/null 2>&1 ; then
+    date --version >/dev/null 2>&1
+    if [[ $? -ne 0 ]];  then
+        # date utility is BSD. Test if gdate is installed
+        if gdate --version >/dev/null 2>&1 ; then
             date() {
                 gdate "$@"
             }
-		else
+        else
             # BSD date
             date() {
                 if [[ "$1" == "-r" ]]; then
@@ -271,6 +273,35 @@ parse() {
 # $7    filename where everything goes
 # returns $7
 createHtmlPage() {
+    local format=$1
+    local postDate=$2
+    local editDate=$3
+    local title="$4"
+    local content="$5"
+    local tagList="$6"
+    local filename="$7"
+    
+    cat "$global_headerFile" > "$filename"
+    echo "<title>$title</title>" >> "$filename"
+    # body divs
+    echo '<div id="divbodyholder">' >> "$filename"
+    echo '<div class="headerholder"><div class="header">' >> "$filename"
+    # blog title
+    echo '<div id="title"><h1 class="nomargin"><a class="ablack" href="'$global_url'">'$global_title'</a></h1>' >> "$filename"
+    echo '<div id="description">'$global_description'</div>' >> "$filename"
+    # title, header, headerholder respectively
+    echo '</div></div></div>' >> "$filename"
+    echo '<div id="divbody"><div class="content">' >> "$filename"
+    
+    # not doing index, just one entry
+    if [[ "$filename" != "$global_indexFile" ]]; then
+        echo '<!-- entry begin -->' >> "$filename" # marks the beginning of the whole post
+        echo '<h3><a class="ablack" href="'$global_url"$(echo $filename | sed "s/$global_htmlDir//")"'">' >> "$filename"
+        # remove possible <p>'s on the title because of markdown conversion
+        echo "$(echo "$title" | sed 's/<\/*p>//g')" >> "$filename"
+        echo '</a></h3>' >> "$filename"
+    fi
+    
     echo $7
 }
 
@@ -341,6 +372,80 @@ backup() {
     chmod 600 $global_backupFile
 }
 
+# if the file does not already exist,
+# creates style sheet from scratch
+#
+# takes no args
+createCss() {
+    # this is basically a line-for-line copy of the original bashblog's css
+    # if you're comparing it to the original, this is the css for 
+    # both the blog.css and main.css files. Some things may not be relevant anymore,
+    # or may be ready to style things that haven't been implemented yet in bashblog2.
+    #
+    # This needs to be reviewed.
+    if [[ ! -f "$global_blogcssFile" ]]; then
+    echo 'body{font-family:Georgia,"Times New Roman",Times,serif;margin:0;padding:0;background-color:#F3F3F3;}
+#divbodyholder{padding:5px;background-color:#DDD;width:874px;margin:24px auto;}
+#divbody{width:776px;border:solid 1px #ccc;background-color:#fff;padding:0px 48px 24px 48px;top:0;}
+.headerholder{background-color:#f9f9f9;border-top:solid 1px #ccc;border-left:solid 1px #ccc;border-right:solid 1px #ccc;}
+.header{width:800px;margin:0px auto;padding-top:24px;padding-bottom:8px;}
+.content{margin-bottom:45px;}
+.nomargin{margin:0;}
+.description{margin-top:10px;border-top:solid 1px #666;padding:10px 0;}
+h3{font-size:20pt;width:100%;font-weight:bold;margin-top:32px;margin-bottom:0;}
+.clear{clear:both;}
+#footer{padding-top:10px;border-top:solid 1px #666;color:#333333;text-align:center;font-size:small;font-family:"Courier New","Courier",monospace;}
+a{text-decoration:none;color:#003366 !important;}
+a:visited{text-decoration:none;color:#336699 !important;}
+blockquote{background-color:#f9f9f9;border-left:solid 4px #e9e9e9;margin-left:12px;padding:12px 12px 12px 24px;}
+blockquote img{margin:12px 0px;}
+blockquote iframe{margin:12px 0px;}
+#title{font-size: x-large;}
+a.ablack{color:black !important;}
+li{margin-bottom:8px;}
+ul,ol{margin-left:24px;margin-right:24px;}
+#all_posts{margin-top:24px;text-align:center;}
+.subtitle{font-size:small;margin:12px 0px;}
+.content p{margin-left:24px;margin-right:24px;}
+h1{margin-bottom:12px !important;}
+#description{font-size:large;margin-bottom:12px;}
+h3{margin-top:42px;margin-bottom:8px;}
+h4{margin-left:24px;margin-right:24px;}
+#twitter{line-height:20px;vertical-align:top;text-align:right;font-style:italic;color:#333;margin-top:24px;font-size:14px;}' > "$global_htmlDir/$global_blogcssFile"
+    fi
+}
+
+# if they do not already exist,
+# creates header and footer from scratch
+#
+# takes no args
+createHeaderFooter() {
+    echo $global_headerFile
+    if [[ ! -f "$global_headerFile" ]]; then
+        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+<link rel="stylesheet" href="'$global_blogcssFile'" type="text/css" />' > "$global_headerFile"
+    fi
+    if [[ ! -f "$global_footerFile" ]]; then
+        local protected_mail="$(echo "$global_email" | sed 's/@/\&#64;/g' | sed 's/\./\&#46;/g')"
+        echo '<div id="footer">'$global_license '<a href="'$global_author_url'">'$global_author'</a> &mdash; <a href="mailto:'$protected_mail'">'$protected_mail'</a><br/>
+Generated with <a href="https://github.com/pointychimp/bashblog2">bashblog2</a>, based on <a href="https://github.com/cfenollosa/bashblog">bashblog</a></div>' > "$global_footerFile"
+    fi
+}
+
+# creates css file(s), makes directories, etc
+#
+# takes no args
+initialize() {
+    detectDateVersion
+    initializeGlobalVariables # initalize and load global variables from config
+    [[ -f "$global_config" ]] && source "$global_config" &> /dev/null
+    mkdir -p "$global_sourceDir" "$global_draftsDir" "$global_htmlDir" "$global_previewDir" "$global_tempDir"
+    createCss
+    createHeaderFooter
+}
+
 # wrapper for logging to $global_logFile
 #
 # $1    stuff to put in log file
@@ -363,13 +468,9 @@ exit() {
 # main (execution starts here)
 ########################################################################
 log "[Info] Starting run"
-detectDateVersion
-initializeGlobalVariables # initalize and load global variables from config
-mkdir -p "$global_sourceDir" "$global_draftsDir" "$global_htmlDir" "$global_previewDir" "$global_tempDir"
-[[ -f "$global_config" ]] && source "$global_config" &> /dev/null
+initialize
 # make sure $EDITOR is set
 [[ -z $EDITOR ]] && echo "Set \$EDITOR enviroment variable" && exit "[Error] \$EDITOR not exported"
-
 # check for valid arguments
 # chain them together like [[  ]] && [[  ]] && ... && usage && exit
 [[ $1 != "edit" ]] && [[ $1 != "post" ]] && usage && exit
